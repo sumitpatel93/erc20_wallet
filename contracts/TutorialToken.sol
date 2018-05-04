9ce55d6365f8dae1ce2de0b4e9ac88fb7325f4e5
Appinventiv <sumit.patel93@gmail.com>
2018-05-04 13:44:24 +0530

    recent trx details

--- contracts/TutorialToken.sol
+++ contracts/TutorialToken.sol
@@ -6,10 +6,16 @@ contract TutorialToken is StandardToken {
     string public name = 'TutorialToken';
     string public symbol = 'TT';
     uint8 public decimals = 18;
-    uint public INITIAL_SUPPLY =  10000 * (10 ** uint256(decimals));
+    uint public INITIAL_SUPPLY =  1000;
+    address public owner;
 
     function TutorialToken() public {
         totalSupply = INITIAL_SUPPLY;
         balances[msg.sender] = INITIAL_SUPPLY;
     }
+    function owner() public {
+          owner = msg.sender;
+
+    }
+   
 }
---
+++ test/test.js
@@ -0,0 +1,16 @@
+'use strict';
+const TutorialToken = artifacts.require('TutorialToken');
+
+contract('TutorialToken', function() {
+    beforeEach(async function() {
+        this.token = await TutorialToken.new();
+    });
+
+    describe('check the total supply of token to be 1000', function() {
+        it('returns the total amount of tokens', async function() {
+            const totalSupply = await this.token.totalSupply();
+            assert.equal(totalSupply, 1000);
+        });
+    });
+
+})
